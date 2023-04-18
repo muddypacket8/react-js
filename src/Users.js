9dec3bbf8fd9b3a9feefad39ad3583baf3fd6ddb
bonnkekimani <bonnke.kimani@student.moringaschool.com>
2023-04-18 13:05:40 +0300

    Users

---
+++ src/Users.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Users = (props) => {
+    return (
+        //What I want to display
+        <div>
+            <p>This is employee {props.name}</p>
+        </div>
+
+    );
+
+}
+export default Users;
