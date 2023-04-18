e546ad2bb79b6ca628f16a1d8d7dd50a0cbca57e
bonnkekimani <bonnke.kimani@student.moringaschool.com>
2023-04-18 13:05:06 +0300

    Boss

---
+++ src/Boss.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Boss = (prop) => {
+    return (
+        //What I want to display
+        <div>
+            <p>This is boss {prop.name}</p>
+        </div>
+
+    );
+
+}
+export default Boss;
